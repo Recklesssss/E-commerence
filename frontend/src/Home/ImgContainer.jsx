f3b80a3e877ad5b130cf81c82ccc53f4bc539ac0
reckless <biruktesfa444@gmail.com>
2024-11-16 15:00:18 +0300

    setting up the responsiveness

---
+++ frontend/src/Home/ImgContainer.jsx
@@ -0,0 +1,54 @@
+const Image = [
+  {
+    img_url: 'https://images.pexels.com/photos/18105/pexels-photo.jpg',
+    name: 'Laptop',
+    categories: 'electronics',
+  },
+  {
+    img_url: 'https://images.pexels.com/photos/1334599/pexels-photo-1334599.jpeg',
+    name: 'Smartphone',
+    categories: 'electronics',
+  },
+  {
+    img_url: 'https://images.pexels.com/photos/1394876/pexels-photo-1394876.jpeg',
+    name: 'Tablet',
+    categories: 'electronics',
+  },
+  {
+    img_url: 'https://images.pexels.com/photos/162489/wireless-mouse-162489.jpeg',
+    name: 'Wireless Mouse',
+    categories: 'electronics',
+  },
+  {
+    img_url: 'https://images.pexels.com/photos/159106/headphones-159106.jpeg',
+    name: 'Over-Ear Headphones',
+    categories: 'electronics',
+  },
+  {
+    img_url: 'https://images.pexels.com/photos/67636/pexels-photo-67636.jpeg',
+    name: 'DSLR Camera',
+    categories: 'electronics',
+  },
+  {
+    img_url: 'https://images.pexels.com/photos/116182/pexels-photo-116182.jpeg',
+    name: 'Mechanical Keyboard',
+    categories: 'electronics',
+  },
+  {
+    img_url: 'https://images.pexels.com/photos/40069/tablet-device-digital-modern-40069.jpeg',
+    name: 'Wireless Earbuds',
+    categories: 'electronics',
+  },
+  {
+    img_url: 'https://images.pexels.com/photos/356743/pexels-photo-356743.jpeg',
+    name: 'Smartwatch',
+    categories: 'electronics',
+  },
+  {
+    img_url: 'https://images.pexels.com/photos/58021/pexels-photo-58021.jpeg',
+    name: '4K Television',
+    categories: 'electronics',
+  },
+];
+
+export default Image;
--- frontend/src/Home/Main/Main.jsx
+++ frontend/src/Home/Main/Main.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
-import "./Main.css"
+import React, { useState, useEffect } from 'react';
+import './Main.css';
+import Image from '../ImgContainer';
 
 function Main() {
+  const [electronics, setElectronics] = useState([]);
+
+  // Use useEffect to set the state after the component mounts
+  useEffect(() => {
+    setElectronics(Image);
+  }, []);  // Empty array ensures this only runs once when the component mounts
+
   return (
     <div className='adj_main'>
       <div className='banner'>
-      <img src="https://img.freepik.com/free-vector/flat-cyber-monday-social-media-cover-template_23-2149098788.jpg?t=st=1731598552~exp=1731602152~hmac=f375bab32c1d18f45f507fc0f1d93506d561e7d79d03f52fff5e7f05f34d7c56&w=996" alt="__" />
+        <img 
+          src="https://img.freepik.com/free-vector/flat-cyber-monday-social-media-cover-template_23-2149098788.jpg?t=st=1731598552~exp=1731602152~hmac=f375bab32c1d18f45f507fc0f1d93506d561e7d79d03f52fff5e7f05f34d7c56&w=996" 
+          alt="Cyber Monday Banner" 
+        />
       </div>
+      
       <div className="electronics">
-
+        <h5>Electronics</h5>
+        <div className="electronics__container">
+          {electronics.length > 0 ? (
+            electronics.map((item, index) => (
+              <div className="electronics__item" key={index}>
+                <img src={item.img_url} alt={item.name} />
+                <p>{item.name}</p>
+              </div>
+            ))
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
       </div>
+      
       <div className="furniture">
-
+        {/* Furniture items can be added here */}
       </div>
+      
       <div className="agriculture">
-        
+        {/* Agriculture items can be added here */}
       </div>
-        
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
